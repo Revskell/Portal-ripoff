f77edd82c64664d030a6a58c34c334c030794905
Pol420 <pfernandezgu@edu.tecnocampus.cat>
2020-11-02 12:49:43 +0100

    portals

---
+++ Assets/Scripts/Portals/PortalCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCam : MonoBehaviour
+{
+
+    [SerializeField] private Transform portal = null;
+    [SerializeField] private Transform otherPortal = null;
+    private Transform player;
+
+    void Start()
+    {
+        player = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        transform.position = portal.position + (player.position - otherPortal.position);
+        transform.rotation = Quaternion.LookRotation(Quaternion.AngleAxis(Quaternion.Angle(portal.rotation, otherPortal.rotation), Vector3.up) * player.forward, Vector3.up);
+    }
+}
---
+++ Assets/Scripts/Portals/PortalTextures.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextures : MonoBehaviour
+{
+
+    [SerializeField] private Camera otherCam = null;
+    [SerializeField] private Material otherMat = null;
+
+    void Start()
+    {
+        if (otherCam.targetTexture != null) otherCam.targetTexture.Release();
+        otherCam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        otherMat.mainTexture = otherCam.targetTexture;
+    }
+}
